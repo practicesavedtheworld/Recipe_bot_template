06a902750dbba5841616707c66ff896d7bb4ca09
practicesavedtheworld <richdaysiscoming@gmail.com>
2024-02-03 12:42:29 +0300

    added schemas

---
+++ database/schemas.py
@@ -0,0 +1,11 @@
+import pathlib
+
+from bson import ObjectId
+from pydantic import BaseModel
+
+
+class FoundRecipeScheme(BaseModel):
+    _id: ObjectId
+    recipe_name: str
+    ingredients: str
+    method: str
