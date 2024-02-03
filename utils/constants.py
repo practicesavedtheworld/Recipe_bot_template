783610d3bed9881079107f0440506b6f8c41e184
practicesavedtheworld <richdaysiscoming@gmail.com>
2024-02-03 12:46:13 +0300

    added some constants

---
+++ utils/constants.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from typing import Literal, TypeAlias
+
+try:
+    LogLevel: TypeAlias = Literal[
+        "NOTSET",
+        "DEBUG",
+        "INFO",
+        "WARNING",
+        "ERROR",
+        "CRITICAL",
+    ]
+    PROJECT_ROOT: Path = Path(__file__).parent.parent
+    try:
+        DEFAULT_RECIPE_IMAGE = f"{PROJECT_ROOT}/static/images/def.png"
+    except FileNotFoundError:
+        # TODO exception + logging
+        print("FILE NOT FOUND")
+except Exception as unexpected_error:
+    # TODO exception + logging
+    print(unexpected_error)
