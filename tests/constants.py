79276f488f2cdd902cbc99e0a3e54eb3572e80a5
practicesavedtheworld <richdaysiscoming@gmail.com>
2024-02-05 14:47:19 +0300

    added type hints

--- tests/constants.py
+++ tests/constants.py
@@ -1,21 +1,21 @@
-from motor.motor_asyncio import AsyncIOMotorClient
+from typing import Any
 
 from database.dao import MongoDAOConnector
 from settings.settings import settings
 
-MONGO_TEST_DOCUMENT = {
+MONGO_TEST_DOCUMENT: dict[str, str] = {
     "k1": "v1",
     "k2": "v2",
     "k3": "v3",
     "k4": "v4",
     "k5": "v5",
 }
-FAKE_COLLECTION = (
+FAKE_COLLECTION: tuple[Any, ...] = (
     [],
     None,
     {},
 )
-TEST_CLIENT_MONGO: AsyncIOMotorClient = MongoDAOConnector(
+TEST_CLIENT_MONGO: MongoDAOConnector = MongoDAOConnector(
     host=settings.DATABASE_HOST_TEST,
     port=int(settings.DATABASE_PORT_TEST),
 )
