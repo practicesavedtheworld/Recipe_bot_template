18e3029546e2a8f27313e951a4651a6505cbf221
practicesavedtheworld <richdaysiscoming@gmail.com>
2024-02-07 14:49:53 +0300

    add venv, mypy dirs ignoring

--- bot/exceptions/bot_exceptions.py
+++ bot/exceptions/bot_exceptions.py
@@ -6,3 +6,8 @@ from exceptions.base import BaseRecipeBotException
 @dataclass
 class FailedLoadBotException(BaseRecipeBotException):
     info: str = "Failed to load bot. WRONG TOKEN or TOKEN FORMAT. CHECK IT"
+
+
+@dataclass
+class StaticFileNotFoundException(BaseRecipeBotException):
+    info: str = "Static file not found. Check path to file"
