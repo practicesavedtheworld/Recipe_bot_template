f50adb672a0cf38599d2d2f24e5e111cdecd3d16
practicesavedtheworld <richdaysiscoming@gmail.com>
2024-02-07 15:21:32 +0300

    up to date

--- utils/constants.py
+++ utils/constants.py
@@ -1,17 +1,9 @@
-import logging
 import os.path
 from pathlib import Path
 from typing import Literal, TypeAlias
 
 from bot.exceptions.bot_exceptions import StaticFileNotFoundException
 from bot.exceptions.unexpected_exceptions import UnexpectedException
-from utils.logger import create_logger
-
-constants_logger = create_logger(
-    loger_name="constants logger",
-    logger_level=logging.ERROR,
-    file_info=Path(__file__).name,
-)
 
 try:
     LogLevel: TypeAlias = Literal[
@@ -25,12 +17,8 @@ try:
     PROJECT_ROOT: Path = Path(__file__).parent.parent
     DEFAULT_RECIPE_IMAGE_PATH = f"{PROJECT_ROOT}/bot/static/images/def.png"
     if not os.path.exists(DEFAULT_RECIPE_IMAGE_PATH):
-        constants_logger.error(
-            f"Some file not found. Path: {DEFAULT_RECIPE_IMAGE_PATH}"
-        )
         raise StaticFileNotFoundException(
             exc_details=f"{DEFAULT_RECIPE_IMAGE_PATH} doesn't exist"
         )
 except Exception as unexpected_error:
-    constants_logger.error(f"Unexpected error. Details: {str(unexpected_error)}")
     raise UnexpectedException(exc_details=str(unexpected_error))
